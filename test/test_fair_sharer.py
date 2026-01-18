747626d3098071c63643f23df0a70fae3b51dbc8
RonaldCrack10 <rashidgaming6@gmail.com>
2026-01-18 20:50:45 +0100

    test

--- test/test_fair_sharer.py
+++ test/test_fair_sharer.py
@@ -1,7 +1,7 @@
-from scr import fairsharer
+from scr.fairsharer import fair_sharer
 def test_fair_sharer():
     
 
-    result = fairsharer.fair_sharer([10, 20, 30, 40, 50, 0], 4, 0.1)
+    result = fair_sharer([10, 20, 30, 40, 50, 0], 4, 0.1)
     expected = [10, 20, 34.0, 38.0, 10.0, 0]
     assert result == expected
