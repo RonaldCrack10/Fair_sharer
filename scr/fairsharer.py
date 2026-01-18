4af9dd22610be36eaa05181934679900fc8b6c30
RonaldCrack10 <rashidgaming6@gmail.com>
2026-01-18 20:26:32 +0100

    Fair Sharer Algorithmus works

---
+++ scr/fairsharer.py
@@ -0,0 +1,21 @@
+
+
+def fair_sharer(values: list, num_iterations: int, share: float) -> list:
+
+    highest_value = max(values)
+    values_new = []
+
+    for index, value in enumerate(values):
+        if values[index] == highest_value:
+            highest_value_index = index
+
+            for i in range(num_iterations):
+                values[highest_value_index + 1] += share * values[highest_value_index]
+                values[highest_value_index - 1] += share * values[highest_value_index]
+
+                
+    return values
+
+
+fair = fair_sharer([10, 20, 30, 40, 50, 0], 4, 0.1)
+print(fair)
